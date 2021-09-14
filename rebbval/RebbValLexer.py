6400e96e9a41856e50a92fe50278ac41b719cd53
william <jinwei@gmail.com>
2021-09-14 14:18:30 +0800

    fix typing.io import issue

--- rebbval/RebbValLexer.py
+++ rebbval/RebbValLexer.py
@@ -1,9 +1,11 @@
 # Generated from RebbVal.g4 by ANTLR 4.9
+import sys
 from antlr4 import *
 from io import StringIO
-from typing import TextIO
-import sys
-
+if sys.version_info[1] > 5:
+	from typing import TextIO
+else:
+	from typing.io import TextIO
 
 
 def serializedATN():
